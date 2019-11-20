a7db39c2c3a026c08d2ad3d10b040a9a3f48dc1d
tsangaaron09 <aaron2018008@concordiashanghai.org>
2019-11-20 15:30:53 -0800

    added comments for clarity

--- rrm/test.py
+++ rrm/test.py
@@ -2,7 +2,7 @@
 """
 @author: Aaron Tsang
 """
-import pywt 
+# import pywt 
 import cmath as math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,12 +14,13 @@ EIIP = [0,0,0.0036,0.005,0.0057,0.0058,0.0198,0.0242,0.0371,0.0373,0.0516,0.0548
 xaxis = []
 cross = []
 
-# amino sequence -> EIIP -> fft
+# FGF amino acid sequence
 seq = ['PALPEDGGSGAFPPGHFKDPKRLYCKNGGFFLRIHPDGRVDGVREKSDPHIKLQLQAEERGWSIKGVCANRYLAMKEDGRLLASKCVTDECFFFERLESNNYNTYRSRKYSSWYVALKRTGQYKLGPKTGPGQKAILFLPMSAKS']
       # 'FNLPLGNYKKPKLLYCSNGGYFLRILPDGTVDGTKDRSDQHIQLQLCAESIGEVYIKSTETGQFLAMDTDGLLYGSQTPNEECLFLERLEENHYNTYISKKHAEKHWFVGLKKNGRSKLGPRTHFGQKAILFLPLPVSSD']
 newpep = []
 result = {}
 
+# scaling the x-axis 
 xaxis = []
 for i in range(0, int((len(seq[0])/2) + 1)):
     xaxis.append((i/len(seq[0])/2)/2)
@@ -30,23 +31,24 @@ newpep = []
 # from amino acids to EIIP value
 for nums in seq[0]:
     newpep.append(EIIP[AA.index(nums)])
-    result[seq[0]] = pywt.dwt(newpep, 'db1')
-
-
-#
-#pep2 = []
-#holder = 0
-#for nums in range(0, len(seq[0])):
-#   if(nums % 2 == 0):
-#        pep2.append(abs(result[seq[0]][nums] * math.exp((1j ** -1) * ((2 * math.pi)/len(seq[0])) * holder * holder).real))
-#        holder = holder + 1
-#        
+    # result[seq[0]] = pywt.dwt(newpep, 'db1') trying out wavelet transform
 
 
 
+# manually computing the DFT 
+pep2 = []
+holder = 0
+for nums in range(0, len(seq[0])):
+   if(nums % 2 == 0):
+        pep2.append(abs(newpep[nums] * math.exp((1j ** -1) * ((2 * math.pi)/len(seq[0])) * holder * holder)).real)
+        holder = holder + 1
+        
+
+
+# finding max and scaling the y-axis from 0 to 100 percent
 final = []
-for amino in result[seq[0]]:
-    final.append(amino/max(result[seq[0]]))
+for amino in pep2:
+    final.append(amino/max(pep2))
 result[seq[0]] = final
     
 
