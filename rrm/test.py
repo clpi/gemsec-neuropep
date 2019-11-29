8f80ecc73235ce2b6c248c993c7eaeaba2253997
tsangaaron09 <aaron2018008@concordiashanghai.org>
2019-11-29 00:08:06 -0800

    update rrm code

--- rrm/test.py
+++ rrm/test.py
@@ -3,7 +3,8 @@
 @author: Aaron Tsang
 """
 # import pywt 
-import cmath as math
+import cmath as cmath
+import math as math
 import numpy as np
 import matplotlib.pyplot as plt
 import statistics as stat
@@ -17,85 +18,29 @@ cross = []
 # FGF amino acid sequence
 seq = ['PALPEDGGSGAFPPGHFKDPKRLYCKNGGFFLRIHPDGRVDGVREKSDPHIKLQLQAEERGWSIKGVCANRYLAMKEDGRLLASKCVTDECFFFERLESNNYNTYRSRKYSSWYVALKRTGQYKLGPKTGPGQKAILFLPMSAKS']
       # 'FNLPLGNYKKPKLLYCSNGGYFLRILPDGTVDGTKDRSDQHIQLQLCAESIGEVYIKSTETGQFLAMDTDGLLYGSQTPNEECLFLERLEENHYNTYISKKHAEKHWFVGLKKNGRSKLGPRTHFGQKAILFLPLPVSSD']
-newpep = []
 result = {}
 
-# scaling the x-axis 
-xaxis = []
-for i in range(0, int((len(seq[0])/2) + 1)):
-    xaxis.append((i/len(seq[0])/2)/2)
-    #xaxis.append(i)
     
 newpep = []
 
 # from amino acids to EIIP value
-for nums in seq[0]:
-    newpep.append(EIIP[AA.index(nums)])
-    # result[seq[0]] = pywt.dwt(newpep, 'db1') trying out wavelet transform
+for nums in range (0, int((len(seq[0]))/2)):
+    newpep.append(EIIP[AA.index(seq[0][nums])])
+result[seq[0]] = (np.fft.rfft(newpep)[1:])
 
-
+xaxis = range(0, len(newpep))
 
 # manually computing the DFT 
 pep2 = []
-holder = 0
-for nums in range(0, len(seq[0])):
-   if(nums % 2 == 0):
-        pep2.append(abs(newpep[nums] * math.exp((1j ** -1) * ((2 * math.pi)/len(seq[0])) * holder * holder)).real)
+holder = 1
+for nums in range (0, len(newpep)):
+        pep2.append(abs(newpep[nums] * cmath.exp((1j ** -1) * ((2 * math.pi)/len(seq[0])) * nums * holder).real ))
         holder = holder + 1
-        
+        value = 0
+newpep = []        
+for nums in range(0, len(pep2)):
+    newpep.append(pep2[nums] / max(pep2))
+pep2 = newpep
 
-
-# finding max and scaling the y-axis from 0 to 100 percent
-final = []
-for amino in pep2:
-    final.append(amino/max(pep2))
-result[seq[0]] = final
-    
-
-#
-#
-plt.plot(xaxis, result[seq[0]])
-#plt.title("with norm = ortho")
-#plt.savefig("test.png")
-
-
-
-    
-
-
-    
-
-## find max
-#max = 0
-#for nums in result[seq[0]]:
-#    if max < nums:
-#        max = nums
-#
-## take the percentage out of 100% 
-#newpep = []
-#for nums in result[seq[0]]:
-#    newpep.append(nums/ max)
-#result[seq[0]] = newpep
-#
-## plot
-#plt.plot(xaxis, result[seq[0]])
-#
-#
-#
-#
-##
-##newpep = []
-##for nums in seq[1]:
-##    newpep.append(EIIP[AA.index(nums)])
-##for i in range (len(seq[1]), len(seq[0])):
-##    newpep.append(0)
-###result[seq[1]] = np.fft.fft(newpep)
-##result[seq[1]] = newpep
-##
-##plt.plot(xaxis, result[seq[1]])
-##    
-##
-#
-#
-#        
-#        
+plt.plot(xaxis, pep2)
+plt.ylim(0,1)
