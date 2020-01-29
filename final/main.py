fe5511d663784566c006dccfaf16221c9d9dffe5
Chris P <chrisp1877@gmail.com>
2020-01-28 21:50:45 -0800

    Actually added skeleton main.py

---
+++ final/main.py
@@ -0,0 +1,21 @@
+'''
+Title: main.py placeholder / skeleton program
+Author: Chris Pecunies
+Description: This is the program that will use the statistical PAM30 and cross-entropy similarity calculations (Savvy) and RRM signal-noise and correlation coefficient calculations (Aaron), as well as unsupervized statistical clustering to determine important sequence domains relative to both biological function mapping (provided in documentation) and EIIP mapping (also in docs). 
+'''
+
+import os
+import pandas as pd
+# from scikit-learn import ( ... )
+def get_input():
+    pass
+
+def process_input(csv_in, *seqs):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
