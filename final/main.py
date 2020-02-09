dbf66288841e42fc325c3631a5642afaa01aeb4e
Chris P <chrisp@localhost.localdomain>
2020-02-09 04:15:40 -0800

    adding methods

--- final/main.py
+++ final/main.py
@@ -1,44 +1,65 @@
 import pandas as pd
+import numpy as np
 from scipy.stats import kendalltau
+import matplotlib.pyplot as plt
 # from scikit-learn import ( ... )
 
---------------------const
+# --------------------const
 
-AA = list('LINGVEPHKAYWQMSCTFRD')
-AA_COL = "AA_seq"   #--> Column title for AA sequences in .csv file
-AA_CHART = pd.read_csv('data/aa_chart.csv')   #--> Path of .csv with AA translations
-AA_EIIP = AA_CHART[['AA', 'EIIP']]
-AA_FNDOM = AA_CHART[['AA', 'Num', 'Function']]
 # get corr between EIIP and Bio Num
 EII_FNDOM_CORR, EII_FNDOM_PVAL = kendalltau(AA_CHART[['Num']], AA_CHART[['EIIP']])
-GRBP = 'IMVTESSDYSSY'
+GRBP5 = 'IMVTESSDYSSY'
 M6 = 'IMVTASSAYDDY'
 PEP_PATH = 'NPS_Numerical_Codes.csv'
 
 #--------------------func
 
 class SequenceSimilarity(object):
+    '''
+    Class that takes in a path to a list of amino acid sequences as well
+    as any number of peptide sequences explicitly that are known to have
+    a certain set of properties. Generates metrics for similarity for each
+    peptide in path and returns domains AA sequence with high similarity
+    '''
 
     def __init__(self, seq_path, *binders):
+
+        self.AA_COL = "AA_seq"   #--> Column title for AA sequences in .csv file
+        self.AA_CHART = pd.read_csv('data/aa_chart.csv')   #--> Path of .csv with AA translations
+        self.AA_EIIP = AA_CHART[['AA', 'EIIP']]
+        self.AA_FNDOM = AA_CHART[['AA', 'Num', 'Function']]
+        self.AA = list('LINGVEPHKAYWQMSCTFRD')
         self.binders = [binder for binder in binders]
         try:
-            self.seq = pd.Dataframe(seq_path, index=AA_COL)
+            self.data = pd.Dataframe(seq_path, index=AA_COL)
         except:
             print("No .csv at that location.")
 
+    def df_filter_subseq(self, data: pd.DataFrame, sub_seq: str, ind: int = None):
+        '''
+        Takes in a subsequence of equal or lesser length to
+        peptides in class peptide dataframe and returns a dataframe
+        containing only those peptides containing the sequence
+        '''
+        if not {*sub_seq}.issubset({*AA}):
+            raise Exception('Invalid subsequence')
+        if ind is None:
+            return self.data.filter() #@TODO Implement the filter here
 
-def df_filter_subseq(data: pd.DataFrame, sub_seq: str, ind: int = None):
-    if not {*sub_seq}.issubset({*AA}):
-        raise Exception('Invalid subsequence')
-    if ind is None:
-        return data.filter()
+        data_filter = data[AA_COL].apply(lambda s: s[ind:len(dom) == dom])
+        data_with_seq = data[data[AA_COL].filter(data_filter)]
+        return data[data[AA_COL].filter(data_filter)]
 
-    def domain_filter(dom, seq, i):
-        return seq[i:len(dom)] == dom
+    def get_PAM30_similarity(self):
+        raise NotImplementedError
 
-    data_filter = data[AA_COL].apply(lambda s: s[ind:len(dom) == dom])
-    data_with_seq = data[data[AA_COL].filter(data_filter)]
-    return data[data[AA_COL].filter(data_filter)]
+    def get_BLOSUM_similarity(self):
+        raise NotImplementedError
+
+    def get_RRM_SN_ratio(self):
+        get_eiip_seq = lambda pep: list(map(lambda aa: AA_EIIP[aa], pep))
+        get_dft_from_eiip = lambda eiip: np.fft.rfft(eiip)[1:]
+        get_cross_spectrum = lambda p1, p2: [x1*x2 for x1, x2 in zip(p1, p2)]
 
 def get_similarity_from_subseq(subseq: str):
 
@@ -70,7 +91,7 @@ def main():
 
 #-------------------main
 def main():
-    pass
+    seq_sim = SequenceSimilarity(PEP_PATH, GRBP5, M6)
 
 if __name__ == '__main__':
     main()
