a1af37ff3e76f33a4eccf4d427eeea577c3b2134
Chris P <chrisp1877@gmail.com>
2020-03-16 15:14:16 -0700

    cleaning up

--- final/main.py
+++ final/main.py
@@ -7,12 +7,14 @@ def main():
         ('M6','IMVTASSAYDDY')
     ]
     AA_COL = 'Sequences'
-    PEP_PATH = './src_data/Sequence_data.csv' 
+    PEP_PATH = './src_data/Sequence_data.csv'
     data = {
         'grbp5_sim' : SeqSimilarity(SEQS[0], DATA, PEP_PATH, AA_COL),
         'm6_sim' : SeqSimilarity(SEQS[1], DATA, PEP_PATH, AA_COL)
     }
     dval = list(data.values())
+
+    print(dval[1].p)
 
 if __name__ == '__main__':
     main()
