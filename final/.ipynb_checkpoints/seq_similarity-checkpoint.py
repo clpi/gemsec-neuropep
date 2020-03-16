63cac20a0082fcf689dcda5e0bac803aeb48471e
Chris P <chrisp1877@gmail.com>
2020-03-15 18:20:12 -0700

    made main.py work

--- final/.ipynb_checkpoints/seq_similarity-checkpoint.py
+++ final/.ipynb_checkpoints/seq_similarity-checkpoint.py
@@ -1,27 +1,51 @@
 '''
-Class to calculate several simialrity metrics for an input list of peptides given a sequence string to compare it to.
-USAGE: 
-1. Create SequenceSimilarityObject({sequence string}, {dictionary of data paths} (to be deprecated soon -- use
-   internal class data), {path to peptides csv}, {column title of sequence values of aforementioned peptides csv})
-   for any number of sequences you want to be compared to the list of peptides
-2. For each object, call object.generate_similarity() to fill out the Dataframe with similarity metrics
-3. To whittle down this similarity matrix to list only those peptides with pattern matching of a minimum length
-   at a matching index in the rerence peptide (henceforth the binder), call object.get_df_with_binder_subseqs(min_length={#})
 @ Author: Chris Pecunies, with help from Savvy Gupta and Aaron Tsang
 @ Date: February 12, 2020
 '''
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Set, Tuple, Dict, List
 from scipy import stats, signal
 from scipy.fftpack import fft, fftshift
-#import sci-kit learn
 import textdistance as td
 from Bio import pairwise2
 from Bio.SubsMat import MatrixInfo as matlist
+from sklearn.preprocessing import MinMaxScaler
+import copy
 
-class SequenceSimilarity:
+class SeqData:
+
+    CONV = {
+        'NUM': [0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 6, 6, 6, 7],
+        'EIIP' : [0.0946, 0.0516, 0.0548, 0.0373, 0.0057, 0.0, 0.0, 0.0823,\
+                 0.0829, 0.0941, 0.0036, 0.0761, 0.0198, 0.005, 0.1263, 0.0058, \
+                 0.0371, 0.0242, 0.0959, 0.0829],
+        'FNS' : ['Aromatic', 'Aromatic', 'Aromatic', 'Hydrophobic', 'Hydrophobic', \
+                'Hydrophobic', 'Hydrophobic', 'Hydrophobic', 'Polar', 'Polar', 'Polar', \
+                'Polar', 'Proline', 'Glycine', 'Charge (-)', 'Charge (-)', 'Charge (+)', \
+                'Charge (+)', 'Charge (+)', 'Excluded'],
+    }
+    # @TODO: Import Biopython dicts of matrices, not read from .csvs
+    MATR = {
+        'PAM30': pd.read_csv('./src_data/pam30.csv', index_col=0).to_dict(),
+        'BLOSUM45': pd.read_csv('./src_data/BLOSUM.csv', index_col=0).to_dict(),
+    }
+    DIST = {
+        'jaro_winkler': (lambda p1, p2: td.jaro_winkler.normalized_similarity(p1, p2)),
+        'needleman_wunsch': (lambda p1, p2: td.needleman_wunsch.normalized_similarity(p1, p2)),
+        'smith_waterman': (lambda p1, p2: td.smith_waterman.normalized_similarity(p1, p2)),
+        'levenshtein': (lambda p1, p2: td.levenshtein.normalized_similarity(p1, p2))
+    }
+    
+    def __init__(self, conv=CONV.keys(), matr=MATR.keys(), dist=DIST.keys()):
+        
+        self.conv = {cnv:self.CONV[cnv] for cnv in self.CONV.keys() if cnv in conv}
+        self.matr = {mtr:self.MATR[mtr] for mtr in self.MATR.keys() if mtr in matr}
+        self.dist = {dst:self.DIST[dst] for dst in self.DIST.keys() if dst in dist}
+        
+class SeqSimilarity:
     '''
     Class that takes in a path to a list of amino acid sequences as well
     as any number of peptide sequences explicitly that are known to have
@@ -46,7 +70,7 @@ class SequenceSimilarity:
         self.sim_sum = lambda p1, p2, m: sum([self.d['matr'][m][a1][a2] for a1, a2 in zip(p1, p2)])
         
         self.seq = aa_col
-        self.sim_cols = ['PAM30', 'BLOSUM45', 'RRM_SN', 'RRM_Corr', 'weighted_matches']
+        self.sim_cols = ['PAM30', 'BLOSUM45', 'RRM_SN', 'RRM_Corr']
         if dists is not None:
             self.sim_cols += self.d['dist'].keys() if not dists else dists
         self.conv_cols = [conv+'_Seq' for conv in self.conv]
@@ -78,34 +102,51 @@ class SequenceSimilarity:
         """
         Uses the Resonant Recognition Model as described by Irena Cosic to 
         """
-        rrm = pd.DataFrame(index=self.p.index)
+        dfrrm = pd.DataFrame(index=self.p.index)
         eiip_seq = self.p['EIIP_Seq'].tolist()
         
-        get_dft_ = lambda e: np.fft.rfft(e)
-        get_dft = lambda e: np.abs(2* np.fft.rfft(e)/len(e))
-        get_freq = lambda e: np.fft.rfftfreq(len(e))[0:int(len(e)/2)+1]
+        get_dft = lambda e: np.fft.rfft(e)
+        get_dft_ = lambda e: np.fft.rfft(e)[1:]**2
+        get_freq = lambda e: np.fft.rfftfreq(len(e))[0:int(len(e)/2)+1][:-1]
         
         self.b_eiip = self.conv_pep('EIIP', self.b)
-        self.b_eiip_dft = get_dft_(self.b_eiip)
-        self.b_dft_conj = np.conj(self.b_eiip_dft)
-        self.b_freq = get_freq(self.b_eiip_dft)
-        self.b_peak = signal.find_peaks(self.b_eiip_dft)
+        self.b_eiip_dft = np.abs(get_dft_(self.b_eiip))
         
-        rrm = rrm.assign(
+        def rrm(seq):
+            if (len(seq)%2) != 0.0:
+                seq = seq + seq[:1]
+            N = len(seq)
+            x = self.conv_pep('EIIP', seq)
+            m = np.reshape(np.linspace(1,int(N),int(N)),[1,int(N)])
+            n = np.reshape(np.linspace(1,int(N/2),int(N/2)),[1,int(N/2)])
+            v = np.matmul(np.transpose(m), n)
+            cx = np.cos(2*np.pi/N*v)
+            sx = np.sin(2*np.pi/N*v)
+            Xre = np.reshape(np.matmul(x,cx),[1,int(N/2)])
+            Xim = np.reshape(np.matmul(x,sx),[1,int(N/2)])
+            A = np.reshape(np.sqrt(Xre**2 + Xim**2),[1,int(N/2)])
+            phase = np.reshape(np.arctan(Xim/Xre),[1,int(N/2)])
+            scaled = 100*(A-min(min(A)))/(max(max(A))-min(min(A)))
+            return n, N, Xre, Xim, A, phase, m, x, scaled
+        
+        rrm_dat = [rrm(seq) for seq in self.p_ls]
+        b_rrm = rrm(self.b)
+        self.b_rrm = copy.deepcopy(b_rrm)
+        
+        dfrrm = dfrrm.assign(
             seq = self.p_ls,
             eiip = eiip_seq,
-            dft = [get_dft_(eiip) for eiip in eiip_seq],
-            freq = [get_freq(eiip) for eiip in eiip_seq],
+            dft = [rrm[2] + rrm[3] for rrm in rrm_dat],
+            cross = [rrm[2] * b_rrm[2] + rrm[3] * b_rrm[3] for rrm in rrm_dat]
         )
-        rrm['cross'] = [d * self.b_dft_conj for d in rrm.dft]
-        rrm['peaks'] = [signal.find_peaks(c)[0] for c in rrm.cross]
-        rrm['SNR'] = [c.mean()/c.std() for c in rrm.cross]
-        rrm['corrcoef'] = [np.corrcoef(d, self.b_eiip_dft)[1][0] for d in rrm.dft]
-        rrm['correlate'] = [signal.correlate(d, self.b_eiip_dft) for d in rrm.dft]
-        rrm['convolve'] = [signal.convolve(d, self.b_eiip_dft) for d in rrm.dft]
-        self.p['RRM_SN'] = rrm['SNR']
-        self.p['RRM_Corr'] = rrm['corrcoef']
-        self.rrm = rrm
+        dfrrm['peaks'] = [np.argmax(c[0]) for c in dfrrm.cross]
+        dfrrm['SNR'] = [max(abs(c[0]))/abs(c[0]).mean() for c in dfrrm.cross]
+        dfrrm['corrcoef'] = [abs(np.corrcoef(d, (b_rrm[2]+b_rrm[3]))[1][0]) for d in dfrrm.dft]
+        dfrrm['correlate'] = [signal.correlate(d, (b_rrm[2]+b_rrm[3])) for d in dfrrm.dft]
+        dfrrm['convolve'] = [signal.convolve(d, (b_rrm[2]+b_rrm[3])) for d in dfrrm.dft]
+        self.p['RRM_SN'] = dfrrm['SNR']
+        self.p['RRM_Corr'] = dfrrm['corrcoef']
+        self.rrm = dfrrm
             
         def plot(seq_i = 0, col = 'dft'): 
             plt.plot(rrm[col].iloc[seq_i])
@@ -117,24 +158,33 @@ class SequenceSimilarity:
     # NOTE! Adds columns "Matching_sseqs" and "Num_matching" to output
     # Might be too unwieldy / unhelpful for output similarity data
     # if so, just comment out _update_matching_sseqs()
-    def _update_matching_sseqs(self, w1: float = 1, w2: float = 1) -> None:
+    def _update_matching_sseqs(self, typ='seq', w1: float = 1, w2: float = 1) -> None:
         """
         Returns a number as a new column representing the number of "matches" a peptide
         has for all possible subsequences for the binder inputted at a given index. For
         weighting=1, all matches are treated equally ('Y' at position 3 is treated equal
         to IMV at position 0) but lowering weighting lowers smaller-length matches
         """
-        self.p['sseq_matches'] = None
-        self.p['weighted_matches'] = None
+        if typ == 'seq':
+            self.p['sseq_matches'] = None
+            self.p['sseq_match_score'] = None
+        if typ == 'num':
+            self.p['num_matches'] = None
+            self.p['num_match_score'] = None
         
         score = lambda s: (w1 * 1) + (len(s)**w2)
+        all_seqs = self.p_ls if typ == 'seq' else list(self.p.NUM_Seq)
         all_sseqs = self.bsseq
         matches = list(); num_matches = list()
-        for i, seq in enumerate(self.p[self.seq]):
+        for i, seq in enumerate(all_seqs):
             matches.append([]); num_matches.append(0)
             lmatch = None
             for j, AA in enumerate(seq):
                 for k, (sseq, bin_i) in enumerate(all_sseqs):
+                    if typ == 'num':
+                        conv = self.conv_pep('NUM', sseq)
+                        conv = "".join([str(num) for num in conv])
+                        sseq = conv
                     if (bin_i == j) and (seq[j:len(sseq)+j] == sseq):
                         if lmatch is not None:
                             if seq[lmatch[1]:lmatch[1]+lmatch[2]].find(seq[j:len(sseq)+j]) >= 0:
@@ -142,10 +192,13 @@ class SequenceSimilarity:
                         if all_sseqs[k-1][1] == bin_i:
                             num_matches[i] -= score(matches[i].pop()[0])
                             lmatch = (sseq, bin_i, len(sseq))
-                        matches[i].append((sseq, bin_i))   
+                        matches[i].append((sseq, bin_i))
                         num_matches[i] += score(sseq)
-
-        self.p.sseq_matches, self.p.weighted_matches = matches, num_matches
+        if typ == 'seq':
+            self.p.sseq_matches, self.p.sseq_match_score = matches, num_matches
+        elif typ == 'num':
+            self.p.num_matches, self.p.num_match_score = matches, num_matches
+        
                 
     def update_similarities(self, metrics: List = []) -> None:
         '''
@@ -154,11 +207,23 @@ class SequenceSimilarity:
         (should be handled automatically)
         '''
         matrices = list(self.d['matr'].keys())
-        cdata = [[self.conv_pep(c, p) for c in self.conv] for p in self.p_ls]
-        mdata = [[self.sim_sum(p, self.b, m) for m in matrices] for p in self.p_ls]
-        self.p[self.conv_cols] = cdata
-        self.p[matrices] = mdata
-        self._update_matching_sseqs()
+        num_seq = [self.conv_pep('NUM', p) for p in self.p_ls]
+        num_out = []
+        for num in num_seq:
+            num = "".join([str(i) for i in num])
+            num_out.append(str(num))
+        self.p['NUM_Seq'] = num_out
+        self.p['EIIP_Seq'] = [self.conv_pep('EIIP', p) for p in self.p_ls]
+        self.p['FNS_Seq'] = [self.conv_pep('FNS', p) for p in self.p_ls]
+        self.p[matrices] = [[self.sim_sum(p, self.b, m) for m in matrices] for p in self.p_ls]
+        scaler = MinMaxScaler((0,1))
+        keys = list(self.d['dist'].keys())
+        blosum = scaler.fit_transform(self.p[['BLOSUM45']].values)
+        pam = scaler.fit_transform(self.p[['PAM30']].values)
+        self.p['BLOSUM45'] = blosum
+        self.p['PAM30'] = pam
+        self._update_matching_sseqs(typ='seq')
+        self._update_matching_sseqs(typ='num')
         self._update_RRM_similarity()
         if metrics is not None:
             dists = metrics if not len(metrics) == 0 else list(self.d['dist'].keys())
@@ -220,44 +285,3 @@ class SequenceSimilarity:
             return dict(zip(top_seq, top_values))
         top_data = self.p[self.p[self.seq].isin(top_seq)]
         return top_data
-                    
-        #@TODO Finish
-                
-                
-    #-------------------miscellaneous methods------------------------------#
-    
-#     def get_kendalltau_corr_map(self) -> Tuple:
-#         return stats.kendalltau(self.data['AA_MAP'][['Num']], self.data['AA_MAP'][['EIIP']])
-
-from Bio.SubsMat import MatrixInfo as matlist
-
-class SeqData:
-
-    CONV = {
-        'NUM': [0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 6, 6, 6, 7],
-        'EIIP' : [0.0946, 0.0516, 0.0548, 0.0373, 0.0057, 0.0, 0.0, 0.0823,\
-                 0.0829, 0.0941, 0.0036, 0.0761, 0.0198, 0.005, 0.1263, 0.0058, \
-                 0.0371, 0.0242, 0.0959, 0.0829],
-        'FNS' : ['Aromatic', 'Aromatic', 'Aromatic', 'Hydrophobic', 'Hydrophobic', \
-                'Hydrophobic', 'Hydrophobic', 'Hydrophobic', 'Polar', 'Polar', 'Polar', \
-                'Polar', 'Proline', 'Glycine', 'Charge (-)', 'Charge (-)', 'Charge (+)', \
-                'Charge (+)', 'Charge (+)', 'Excluded'],
-    }
-    # @TODO: Import Biopython dicts of matrices, not read from .csvs
-    MATR = {
-        'PAM30': pd.read_csv('./src_data/pam30.csv', index_col=0).to_dict(),
-        'BLOSUM45': pd.read_csv('./src_data/BLOSUM.csv', index_col=0).to_dict(),
-    }
-    DIST = {
-        'jaro_winkler': (lambda p1, p2: td.jaro_winkler.normalized_similarity(p1, p2)),
-        'needleman_wunsch': (lambda p1, p2: td.needleman_wunsch.normalized_similarity(p1, p2)),
-        'smith_waterman': (lambda p1, p2: td.smith_waterman.normalized_similarity(p1, p2)),
-        'levenshtein': (lambda p1, p2: td.levenshtein.normalized_similarity(p1, p2))
-    }
-    
-    def __init__(self, conv=CONV.keys(), matr=MATR.keys(), dist=DIST.keys()):
-        
-        self.conv = {cnv:self.CONV[cnv] for cnv in self.CONV.keys() if cnv in conv}
-        self.matr = {mtr:self.MATR[mtr] for mtr in self.MATR.keys() if mtr in matr}
-        self.dist = {dst:self.DIST[dst] for dst in self.DIST.keys() if dst in dist}
-    
--- final/main.py
+++ final/main.py
@@ -1,100 +1,18 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from typing import Set, Tuple, Dict, List
-from scipy import interpolate, stats, fftpack, signal
-#import sci-kit learn
-import textdistance as td
-#import biopython as bio
-
+from seq_similarity import SeqData, SeqSimilarity
 
 def main():
-# Consider using Organized NPS from Savvy's folder?
-    DATA_PATHS = {
-        "BLOSUM":"src_data/BLOSUM.csv",
-        "PAM30":"src_data/pam30.csv",
-    }
+    DATA = SeqData()
     SEQS = [
         ('GRBP5','IMVTESSDYSSY'),
         ('M6','IMVTASSAYDDY')
     ]
     AA_COL = 'Sequences'
-    PEP_PATH = 'src_data/Sequence_data.csv' 
-    dat1 = {
-        'grbp5_sim' : SequenceSimilarity(SEQS[0], DATA_PATHS, PEP_PATH, AA_COL, True, False),
-        'm6_sim' : SequenceSimilarity(SEQS[1], DATA_PATHS, PEP_PATH, AA_COL, True, False),
-        'grbp5_sim_match' : SequenceSimilarity(SEQS[0], DATA_PATHS, PEP_PATH, AA_COL, True, True),
-        'm6_sim_match' : SequenceSimilarity(SEQS[1], DATA_PATHS, PEP_PATH, AA_COL, True, True),
+    PEP_PATH = './src_data/Sequence_data.csv' 
+    data = {
+        'grbp5_sim' : SeqSimilarity(SEQS[0], DATA, PEP_PATH, AA_COL),
+        'm6_sim' : SeqSimilarity(SEQS[1], DATA, PEP_PATH, AA_COL)
     }
-    dat2 = {
-        'both_sep_sim' : dat1['grbp5_sim'].merge_data(other=dat1['m6_sim'], sep_cols=True),
-        'both_avg_sim' : dat1['grbp5_sim'].merge_data(other=dat1['m6_sim'], sep_cols=False),
-        'both_sep_sim_match' : dat1['grbp5_sim_match'].merge_data(other=dat1['m6_sim_match'], sep_cols=True),
-        'both_avg_sim_match' : dat1['grbp5_sim_match'].merge_data(other=dat1['m6_sim_match'], sep_cols=False),
-    }
+    dval = list(data.values())
 
-    d1 = list(dat1.values())
-    d1d = [d.pep_data for d in d1]
-    d2d = list(dat2.values())
-    print([len(d) for d in d1d] + [len(d) for d in d2d])
-
-    for name in dat1.keys():
-        path = "../out/v2/{}_samelen_allmetrics_v2.csv".format(name)
-        dat1[name].pep_data.to_csv(path)
-    for i, name in enumerate(dat2.keys()):
-        if i == 1 or 1 == 3:
-            cols="sep_cols"
-            path = "../out/v2/{}_samelen_allmetrics_v2_{}.csv".format(name, cols)
-            dat2[name].to_csv(path)
-        if i == 0 or i == 2:
-            cols="avg cols"
-            path2 = "../out/v2/{}_samelen_allmetrics_combined_m6_grbp5_{}.csv".format(name,cols)
-            dat2[name].to_csv(path2)
-
-    new1 = dat2['both_avg_sim_match'].groupby('FNS_Seq').mean()
-    new2 = dat2['both_avg_sim_match'].groupby('Sequences')
-
-    dat2['both_avg_sim_match'].describe()
-
-    import seaborn
-    import scipy.signal
-
-    pd.options.display.max_rows = 999
-    pd.set_option('mode.sim_interactive', True)
-    pd.set_option('expand_frame_repr', True)
-    pd.set_option('large_repr', 'truncate')
-
-    for name in dat1.keys():
-        d = dat1[name].pep_data #.plot()
-        #print(d.head())
-        #df = d.groupby('NUM_Seq').plot()
-        #ds = d.sort_values(by="weighted_matches").plot()
-        #df.head(10)
-        #dat1[name].pep_data.hist()
-    #dat2['both_avg_sim_match'][[*distances.keys()] + ["NUM_Seq"]].groupby("NUM_Seq").mean().plot()
-    d1 = list(dat1.keys())
-    d2 = list(dat2.keys())
-
-    dat2[d2[0]][['sseq_matches_GRBP5', 'sseq_matches_M6']]
-
-    def get_RRM():
-        data = similarity.pep_data
-        matrix = similarity.data['PA]
-        print(data.head(), len(data))
-
-    get_RRM()
-
-    SEQS = [
-        ('GRBP5','IMVTESSDYSSY'), #->, -----22---220
-        ('M6','IMVTASSAYDDY')    #->  -----22---550
-    ]
-    s1 = np.fft.rfft(list(dat1['grbp5_sim'].pep_data['EIIP_Seq'].iloc[1]))
-    s1 = np.fft.rfft(list(dat1['grbp5_sim'].pep_data['EIIP_Seq'].iloc[2]))
-    s1 = np.fft.rfft(list(dat1['grbp5_sim'].pep_data['EIIP_Seq'].iloc[3]))
-    plt.plot(np.real(s1))
-    plt.show()
-
-    sorted_grbp5_sim_match = data['grbp5_sim_match'].pep_data.sort_values(by="weighted_matches")
-                                 
 if __name__ == '__main__':
     main()
